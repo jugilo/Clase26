207d6566a37449630b2502b1415d94cde4bcc1f8
jugilo <112008137+jugilo@users.noreply.github.com>
2023-10-26 10:10:53 -0500

    Se agrega backend

---
+++ backendClase26/backendClase/app/Http/Controllers/registroController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\registro;
+use Carbon\Carbon;
+
+class registroController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $registros = registro::all();
+        return $registros;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $registro = new registro();
+        $registro->nombre = $request->nombre;
+        $registro->apellido = $request->apellido;
+        $registro->edad = $request->edad;
+        $registro->cantidadAcompanantes = $request->cantidadAcompanantes;
+        $registro->horaIngreso = Carbon::now();
+        $registro->save();
+        return "Registro guardado correctamente";
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $registro = registro::find($id);
+        $registro->delete();
+        return "Registro eliminado correctamente";
+    }
+}
---
+++ backendClase26/backendClase/database/migrations/2023_10_26_142956_create_registros_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('registros', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->Integer('edad');
+            $table->time('horaIngreso');
+            $table->Integer('cantidadAcompanantes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('registros');
+    }
+};
